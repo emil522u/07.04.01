774befa110d7dbe73afb90eab3f7bca9cb605b4b
Frederik Petersen <frederikpetersen@Frederiks-MBP-6.lan>
2020-09-16 11:44:24 +0200

    al JS og al CSS smidt i hver deres filer

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM loaded');
     navSlide();
+    hentData();
+
 });
+
+
 
 const navSlide = () => {
     const burger = document.querySelector('.burger');
@@ -25,4 +29,73 @@ const navSlide = () => {
         burger.classList.toggle('toggle');
     });
 
+}
+
+let filter = "alle";
+let destinationer;
+let container = document.querySelector("#container");
+let temp = document.querySelector("template");
+
+const link = "https://spreadsheets.google.com/feeds/list/1KumHIMr-aP9Ey9WOiRaoOIaJnTZE0NDR1AoZhw2W5W8/od6/public/values?alt=json"
+
+
+
+async function hentData() {
+    const respons = await fetch(link);
+    destinationer = await respons.json();
+    addEventListenersToButtons();
+    vis(destinationer);
+}
+
+function vis(destinationer) {
+    //løb igennem array "destinationer"
+    container.innerHTML = ""
+    destinationer.feed.entry.forEach(dest => {
+        if (filter == "alle" || filter == dest.gsx$kategori.$t.toLowerCase()) {
+            console.log(dest);
+            const klon = temp.cloneNode(true).content;
+            klon.querySelector(".navn").textContent = dest.gsx$navn.$t;
+            klon.querySelector("img").src = "billeder/" + dest.gsx$billede.$t + ".jpg";
+            klon.querySelector(".kort").textContent = dest.gsx$kort.$t;
+            klon.querySelector(".pris").textContent = "Pris: " + dest.gsx$pris.$t + ",-";
+
+            klon.querySelector("article").addEventListener("click", () => visDetaljer(dest));
+
+            container.appendChild(klon);
+
+        }
+
+    })
+
+}
+
+function visDetaljer(dest) {
+    popup.style.display = "block";
+    popup.querySelector(".navn").textContent = dest.gsx$navn.$t;
+    popup.querySelector(".beskrivelse").textContent = dest.gsx$lang.$t;
+    popup.querySelector("img").src = "billeder/" + dest.gsx$billede.$t + ".jpg";
+
+}
+
+document.querySelector("#luk").addEventListener("click", () => popup.style.display = "none");
+
+
+function addEventListenersToButtons() {
+    document.querySelectorAll(".filter").forEach((btn) => {
+        btn.addEventListener("click", filterBTNs);
+    });
+};
+
+function filterBTNs() {
+    filter = this.dataset.køn;
+    document.querySelector("h1").textContent = this.textContent;
+    document.querySelectorAll(".filter").forEach((btn) => {
+
+
+        btn.classList.remove("valgt");
+
+    });
+
+    this.classList.add("valgt");
+    vis(destinationer);
 }
