0b15dc0a3429d588081215b214eed79ffe18ca28
Jean Wong <jeanwong@Jeans-MacBook-Pro.local>
2020-09-17 12:02:36 +0200

    new

--- script.js
+++ script.js
@@ -58,10 +58,10 @@ function vis(destinationer) {
             const klon = temp.cloneNode(true).content;
             klon.querySelector(".navn").textContent = dest.gsx$navn.$t;
             klon.querySelector("img").src = "billeder/" + dest.gsx$billede.$t + ".jpg";
-            klon.querySelector(".indbyggertal").textContent = dest.gsx$indbyggertal.$t;
-            klon.querySelector(".vejr").textContent = dest.gsx$vejr.$t;
-            klon.querySelector(".funfact").textContent = dest.gsx$funfact.$t;
-            klon.querySelector(".pris").textContent = dest.gsx$pris.$t;
+            klon.querySelector(".indbyggertal").textContent += dest.gsx$indbyggertal.$t;
+            klon.querySelector(".vejr").textContent += dest.gsx$vejr.$t;
+            klon.querySelector(".funfact").textContent += dest.gsx$funfact.$t;
+            klon.querySelector(".pris").textContent += dest.gsx$pris.$t;
 
             klon.querySelector("article").addEventListener("click", () => visDetaljer(dest));
 
